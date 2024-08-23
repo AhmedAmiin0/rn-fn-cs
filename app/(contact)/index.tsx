05e2353ec7b8477eff86d0ff665edd0aa48983af
Yusef Nsar <yousef.ibrahim@cibeg.com>
2024-08-24 01:38:56 +0300

    changes

--- app/(contact)/index.tsx
+++ app/(contact)/index.tsx
@@ -14,32 +14,24 @@ import { Paragraph } from '@/components/Paragraph';
 export default function ContactScreen() {
   return (
     <SafeAreaView>
-      <ScrollView
-        style={{ paddingLeft: 24, paddingRight: 24, paddingTop: 24 }}
-      >
-        <BitcoinSection
-        ></BitcoinSection>
+      <ScrollView style={{ paddingLeft: 24, paddingRight: 24, paddingTop: 24 }}>
+        <BitcoinSection></BitcoinSection>
         <View style={{ flexDirection: 'row', gap: 8, marginBottom: 24 }}>
-          <StatisticsCard isPrimary
-            title='10k'
-            subtitle='limited edition Contracts'
-          />
+          <StatisticsCard isPrimary title='10k' subtitle='limited edition Contracts' />
           <StatisticsCard title='2,979' subtitle='owners' />
           <StatisticsCard title='7.5k' subtitle='sold' />
         </View>
         <View style={{ flexDirection: 'row', gap: 8, marginBottom: 24, height: 91, flex: 1 }}>
-          <Image source={require('../../assets/images/section.png')}
+          <Image
+            source={require('../../assets/images/section.png')}
             resizeMode='contain'
             style={{ flex: 1, height: 91, width: 91, borderRadius: 16 }}
           />
         </View>
         <View>
-          <Paragraph
-
-          >
-            Activate and flaunt 'Bitcoin' name on your phone in under 5 mins
-          </Paragraph>
+          <Paragraph>Activate and flaunt 'Bitcoin' name on your phone in under 5 mins</Paragraph>
         </View>
+        
 
         {/* <Title style={{ marginBottom: 16 }}>
           Key benefits
@@ -49,4 +41,3 @@ export default function ContactScreen() {
     </SafeAreaView>
   );
 }
-
